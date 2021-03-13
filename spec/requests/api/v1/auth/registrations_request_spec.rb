687585fbfe31cdea265288da9773e1b26175e11a
hamabehayato <dogabon35@gmail.com>
2021-03-14 08:27:32 +0900

    devise-token-auth のテストを追加。

--- spec/requests/api/v1/auth/registrations_request_spec.rb
+++ spec/requests/api/v1/auth/registrations_request_spec.rb
@@ -53,4 +53,92 @@ RSpec.describe "Api::V1::Auth::Registrations", type: :request do
       end
     end
   end
+    describe "POST api/v1/auth/sign_in" do
+        subject { post(api_v1_user_session_path, params: params) }
+
+        context "存在する値のとき" do
+            let(:params) { attributes_for(:user, name: user.name, email: user.email, password: user.password) }
+            let(:user) { create(:user) }
+
+            it "ログインできる" do
+                subject
+                res = JSON.parse(response.body)
+                expect(response.headers["uid"]).to be_present
+                expect(response.headers["access-token"]).to be_present
+                expect(response.headers["client"]).to be_present
+                expect(response).to have_http_status(200)
+            end
+        end
+
+        # context "存在しない値のとき" do
+        #     let(:params) { attributes_for(:user) }
+        #     let(:user) { create(:user) }
+
+        #     fit "ログインできない" do
+        #         subject
+        #         expect(response).to have_http_status(401)
+        #     end
+        # end
+
+        context "emailが一致しないとき" do
+            let(:user) { create(:user) }
+            let(:params) { attributes_for(:user, email: "hoge", password: user.password) }
+      
+            it "ログインできない" do
+              subject
+              res = JSON.parse(response.body)
+              header = response.header
+              expect(res["errors"]).to include "Invalid login credentials. Please try again."
+              expect(header["access-token"]).to be_blank
+              expect(header["client"]).to be_blank
+              expect(headers["uid"]).to be_blank
+              expect(response).to have_http_status(:unauthorized)
+            end
+          end
+
+          context "passwordが一致しない場合" do
+            let(:user) { create(:user) }
+            let(:params) { attributes_for(:user, email: user.email, password: "hoge") }
+      
+            it "ログインできない" do
+              subject
+              res = JSON.parse(response.body)
+              header = response.header
+              expect(res["errors"]).to include "Invalid login credentials. Please try again."
+              expect(header["access-token"]).to be_blank
+              expect(header["client"]).to be_blank
+              expect(headers["uid"]).to be_blank
+              expect(response).to have_http_status(:unauthorized)
+            end
+          end
+    end
+
+    describe "DELETE api/v1/auth/sign_out" do
+        subject { delete(destroy_api_v1_user_session_path, headers: headers) }
+        context "ユーザがログインしているとき" do
+            let(:user) { create(:user) }
+            let(:headers) { create(:user).create_new_auth_token }
+            
+            it "ログアウトできる" do
+                subject
+                # expect(user.reload.tokens).to be_blank
+                expect { subject }.to change { user.reload.tokens }.from(be_present).to(be_blank)
+                expect(response).to have_http_status(200)
+            end
+        end
+
+        context "誤った情報を送信したとき" do
+            let(:user) { create(:user) }
+            let!(:token) { user.create_new_auth_token }
+            let!(:headers) { { "access-token" => "", "token-type" => "", "client" => "", "expiry" => "", "uid" => "" } }
+
+            fit "ログアウトできない" do
+                subject
+                expect(response).to have_http_status(:not_found)
+                res = JSON.parse(response.body)
+                expect(res["errors"]).to include "User was not found or was not logged in."
+            end
+        end
+    end
+    
 end
