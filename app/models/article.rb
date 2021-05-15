046989a5fbeeb8d601ce04ea74579a39f36aabbc
hamabehayato <be_dash1122@icloud.com>
2021-05-15 10:14:44 +0900

    文字数制限緩和＆プロジェクトタイトル変更

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 30 }
-  validates :body,  presence: true, length: { maximum: 400 }
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :body,  presence: true
 
   belongs_to :user
   has_many :comments, dependent: :destroy
